c0497cec0d572d952a6d7c63f2fc9d5a3d439279
Cindy San <cindysan97@gmail.com>
2020-07-07 02:58:20 -0700

    Initial commit

---
+++ index_365day.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm_365day'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data_365day.json'
+})
---
+++ index_365home.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm_365home'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data_365home.json'
+  })
